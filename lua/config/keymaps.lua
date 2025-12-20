131852b7ad7913e6dabd43ca52d1aa9a2ce8e57e
Charlie Huang <charliehuang09@gmail.com>
2025-12-20 03:04:12 +0000

    add window switching with ctrl hjkl
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -66,3 +66,8 @@ vim.keymap.set('n', '<C-Right>', '<C-w>l', { desc = "Window right" })
 vim.keymap.set('n', '<leader>fmd', vim.lsp.buf.format)
 
 vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true }) -- Move to the left window
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true }) -- Move to the window below
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true }) -- Move to the window above
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true }) -- Move to the right window
